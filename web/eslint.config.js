25894e439020b03d9eafa77f964d90b552f65f48
Lucie Delestre <me@luclu7.fr>
2024-12-21 23:57:35 +0100

    Mise à jour eslint

---
+++ web/eslint.config.js
@@ -0,0 +1,36 @@
+import reactRefresh from "eslint-plugin-react-refresh";
+import storybook from "eslint-plugin-storybook";
+import { FlatCompat } from "@eslint/eslintrc";
+
+import js from "@eslint/js";
+import typescript from "typescript-eslint";
+
+const compat = new FlatCompat();
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    ignores: [
+      "**/*dist",
+      "storybook-static/",
+      "!.storybook",
+      "**/.eslintrc.cjs",
+    ],
+  },
+  js.configs.recommended,
+  reactRefresh.configs.recommended,
+  ...typescript.configs.recommended,
+  ...storybook.configs["flat/recommended"],
+  ...compat.extends("plugin:react-hooks/recommended"),
+  {
+    rules: {
+      "react-refresh/only-export-components": [
+        "warn",
+        {
+          allowConstantExport: true,
+        },
+      ],
+    },
+  },
+  {},
+];
