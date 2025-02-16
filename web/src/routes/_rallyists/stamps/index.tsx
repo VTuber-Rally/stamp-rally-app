3f83f1a4a81d6ffb5a1d0f6424d1e7346295ea1a
Leon <me@leonekmi.fr>
2025-02-16 17:23:15 +0100

    fix: remove "hello" route

--- web/src/routes/_rallyists/stamps/index.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/_rallyists/stamps/")({
-  component: ScannedQRCodeList,
-});
-
-function ScannedQRCodeList() {
-  return "hello";
-}
