c480adc76e22eb31eff2c7720be342f79621feca
Lucie Delestre <me@luclu7.fr>
2025-05-25 20:55:00 +0200

    Adaption du script de génération des artistes

--- scripts/create-profile.tsx
+++ scripts/create-profile.tsx
@@ -58,28 +58,36 @@ const content = fs.readFileSync(filePath, "utf8");
 const lines = content
   .split("\n")
   .map((line) => line.split("\t"))
+  .filter((line) => {
+    return line[0] !== "Your booth name";
+  })
   .map((line) => {
     return {
-      boothName: line[0].trim(),
-      boothNumber: line[1].trim(),
-      hall: line[2].trim(),
-      personalSetOrder: line[3].trim(),
-      cost: line[4].trim(),
-      artistIn2024Edition: line[5].trim(),
-      status: line[6].trim(),
-      rewardCard: line[7].trim(),
-      comments: line[8].trim(),
-      email: line[9].trim(),
-      artistName: line[10].trim(),
-      goodsDescription: line[11].trim(),
-      promotionalLink: line[12].trim(),
-      topVTubers: line[13].trim(),
-      timestamp: line[14].trim(),
-      filename: line[15].trim(),
+      boothName: line[0]?.trim(),
+      boothNumber: line[1]?.trim(),
+      hall: line[2]?.trim(),
+      personalSetOrder: line[3]?.trim(),
+      cost: line[4]?.trim(),
+      artistIn2024Edition: line[5]?.trim(),
+      status: line[7]?.trim(),
+      spokenLanguages: line[8]?.trim(),
+      hasProvidedPromoAssets: line[9]?.trim(),
+      comments: line[10]?.trim(),
+      algorithmicRewardCard: line[11]?.trim(),
+      artisticRewardCard: line[12]?.trim(),
+      topVTubers: line[13]?.trim(),
+      finalRewardCard: line[14]?.trim(),
+      promotionalLink: line[15]?.trim(),
+      email: line[16]?.trim(),
+      artistName: line[17]?.trim(),
+      goodsDescription: line[18]?.trim(),
+      timestamp: line[19]?.trim(),
+      filename: line[20]?.trim(),
+      rewardCardFile: line[21]?.trim(),
     } satisfies Line;
   }) as Line[];
 
-//   Your booth name	Booth number	Hall	Personal set order	Cost	Artist in the 2024 edition	Status	Reward card	Comments	Adresse e-mail	Your artist name	Please describe your Vtuber-related goods available on your booth (kind of goods and characters/agency...).	Please give one link for your promotional purposes	The top 3 Vtubers you wanna draw for the postcard, by priority order (top 1, top 2, top 3)	Horodateur	Profile picture filename
+// Your booth name	Booth number	Hall	Personal set order	Cost	Artist in the 2024 edition	Promo art	Status	Spoken languages	Has provided promo assets	Comments	"Algorithmic" reward card	"Artistic" / Sedeto's pick reward card	The top 3 Vtubers you wanna draw for the postcard, by priority order (top 1, top 2, top 3)	Final reward card	Please give one link for your promotional purposes	Adresse e-mail	Your artist name	Please describe your Vtuber-related goods available on your booth (kind of goods and characters/agency...).	Horodateur	Profile picture filename	Reward card file
 
 type Line = {
   boothName: string;
@@ -89,15 +97,20 @@ type Line = {
   cost: string;
   artistIn2024Edition: string;
   status: string;
-  rewardCard: string;
+  spokenLanguages: string;
+  hasProvidedPromoAssets: string;
   comments: string;
+  algorithmicRewardCard: string;
+  artisticRewardCard: string;
+  topVTubers: string;
+  finalRewardCard: string;
+  promotionalLink: string;
   email: string;
   artistName: string;
   goodsDescription: string;
-  promotionalLink: string;
-  topVTubers: string;
   timestamp: string;
   filename: string;
+  rewardCardFile: string;
 };
 
 console.log(`Found ${lines.length} profiles`);
@@ -119,59 +132,67 @@ async function createProfilesAndDocuments() {
         : line.email.replace("@", "+").concat("@fake.email");
       const password = generatePassword();
 
-      const newAccount = await users.create(
-        sdk.ID.unique(),
-        emailModified,
-        undefined,
-        password,
-        line.boothName,
-      );
+      try {
+        const newAccount = await users.create(
+          sdk.ID.unique(),
+          emailModified,
+          undefined,
+          password,
+          line.boothName,
+        );
 
-      const { privateKey, publicKey } = await crypto.subtle.generateKey(
-        { name: "ECDSA", namedCurve: "P-384" },
-        true,
-        ["sign", "verify"],
-      );
+        const { privateKey, publicKey } = await crypto.subtle.generateKey(
+          { name: "ECDSA", namedCurve: "P-384" },
+          true,
+          ["sign", "verify"],
+        );
 
-      const exportedPrivateKey = await crypto.subtle.exportKey(
-        "jwk",
-        privateKey,
-      );
-      const exportedPublicKey = await crypto.subtle.exportKey("jwk", publicKey);
+        const exportedPrivateKey = await crypto.subtle.exportKey(
+          "jwk",
+          privateKey,
+        );
+        const exportedPublicKey = await crypto.subtle.exportKey(
+          "jwk",
+          publicKey,
+        );
 
-      await users.updatePrefs(newAccount.$id, {
-        privateKey: JSON.stringify(exportedPrivateKey),
-        publicKey: JSON.stringify(exportedPublicKey),
-      });
+        await users.updatePrefs(newAccount.$id, {
+          privateKey: JSON.stringify(exportedPrivateKey),
+          publicKey: JSON.stringify(exportedPublicKey),
+        });
 
-      await users.updateLabels(newAccount.$id, [
-        "standist",
-        ...(isProduction ? [] : ["test"]),
-      ]);
-      await users.updateEmailVerification(newAccount.$id, true);
+        await users.updateLabels(newAccount.$id, [
+          "standist",
+          ...(isProduction ? [] : ["test"]),
+        ]);
+        await users.updateEmailVerification(newAccount.$id, true);
 
-      console.log(`\`${emailModified}\`:\`${password}\``);
+        console.log(`\`${emailModified}\`:\`${password}\``);
 
-      let imageId = "fallback";
-      if (line.filename) {
-        const imagePath = path.join(userMediasPath, line.filename);
-        if (!fs.existsSync(imagePath)) {
-          throw new Error(`Image file not found: ${imagePath}`);
+        let imageId = "fallback";
+        if (line.filename) {
+          const imagePath = path.join(userMediasPath, line.filename);
+          if (!fs.existsSync(imagePath)) {
+            throw new Error(`Image file not found: ${imagePath}`);
+          }
+          console.log(`Uploading image: ${imagePath}`);
+          imageId = await uploadUserMedia(imagePath);
         }
-        console.log(`Uploading image: ${imagePath}`);
-        imageId = await uploadUserMedia(imagePath);
-      }
 
-      return {
-        userId: newAccount.$id,
-        boothNumber: line.boothNumber,
-        name: line.boothName,
-        hall: line.hall,
-        description: line.goodsDescription,
-        publicKey: JSON.stringify(exportedPublicKey),
-        image: imageId,
-        twitch: line.promotionalLink,
-      } satisfies Partial<StandistDocument>;
+        return {
+          userId: newAccount.$id,
+          boothNumber: line.boothNumber,
+          name: line.boothName,
+          hall: line.hall,
+          description: line.goodsDescription,
+          publicKey: JSON.stringify(exportedPublicKey),
+          image: imageId,
+          twitch: line.promotionalLink,
+        } satisfies Partial<StandistDocument>;
+      } catch (error) {
+        console.error(`Error creating account for ${emailModified}: ${error}`);
+        return null;
+      }
     },
   );
 
