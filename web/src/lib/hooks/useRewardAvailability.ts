b858f17303563cd49fae0fc6a87105f2fcea2116
Leon <me@leonekmi.fr>
2025-06-15 21:05:14 +0200

    feat: add minor hall stamp requirement

--- web/src/lib/hooks/useRewardAvailability.ts
+++ web/src/lib/hooks/useRewardAvailability.ts
@@ -1,20 +1,35 @@
+import { useMemo } from "react";
+
 import {
   premiumRewardMinStampsRequirement,
   standardRewardMinStampsRequirement,
 } from "@/lib/consts.ts";
 import { useCollectedStamps } from "@/lib/hooks/useCollectedStamps.ts";
+import { useStandists } from "@/lib/hooks/useStandists.ts";
 
 export const useRewardAvailability = () => {
   const { data: stamps } = useCollectedStamps();
+  const { data: standists } = useStandists();
+
+  const minorHallStandistsIds = useMemo(() => {
+    const minorHallStandists = standists.filter((standist) => {
+      return standist.hall === "5A";
+    });
+    return new Set(minorHallStandists.map((standist) => standist.userId));
+  }, [standists]);
 
   const stampCount = stamps?.length ?? 0;
+  const isAnyStampFromMinorHall =
+    stamps?.some((stamp) => minorHallStandistsIds.has(stamp.standistId)) ??
+    false;
   const isStandardRewardObtainable =
-    stampCount >= standardRewardMinStampsRequirement;
+    isAnyStampFromMinorHall && stampCount >= standardRewardMinStampsRequirement;
   const isPremiumRewardObtainable =
-    stampCount >= premiumRewardMinStampsRequirement;
+    isAnyStampFromMinorHall && stampCount >= premiumRewardMinStampsRequirement;
 
   return {
     stampCount,
+    isAnyStampFromMinorHall,
     isStandardRewardObtainable,
     isPremiumRewardObtainable,
   } as const;
