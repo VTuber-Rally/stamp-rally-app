e267f4ae582ac722070c35d235b693bc89ab4992
Leon <me@leonekmi.fr>
2025-06-17 23:15:37 +0200

    wip

--- web/src/lib/hooks/useWheelEntries.ts
+++ web/src/lib/hooks/useWheelEntries.ts
@@ -2,12 +2,13 @@ import { useQuery } from "@tanstack/react-query";
 
 import { QUERY_KEYS } from "@/lib/QueryKeys.ts";
 import { useDatabase } from "@/lib/hooks/useDatabase.ts";
+import { RewardDrawType } from "@/lib/rewards.ts";
 
-export const useWheelEntries = () => {
+export const useWheelEntries = (type: RewardDrawType = "standard") => {
   const { getWheelEntries } = useDatabase();
 
   return useQuery({
     queryKey: [QUERY_KEYS.WHEEL_ENTRIES],
-    queryFn: () => getWheelEntries(),
+    queryFn: () => getWheelEntries(type),
   });
 };
