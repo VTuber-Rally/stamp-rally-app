d2617222e377b80cc922b6d8b330d9bf7a0cfa06
Lucie Delestre <me@luclu7.fr>
2025-07-04 08:30:57 +0200

    GroupeInfo: passage à details pour cacher les détails du groupe

--- web/src/components/routes/staff/inventory/GroupInfo.tsx
+++ web/src/components/routes/staff/inventory/GroupInfo.tsx
@@ -16,13 +16,13 @@ export function GroupInfo({
   const { t } = useTranslation();
 
   return (
-    <div className="border-b border-gray-200 bg-gray-50 p-3">
-      <div className="text-center">
-        <h2 className="text-lg font-semibold text-gray-800">
+    <div className="border-b border-gray-200 bg-gray-50 p-3 text-center">
+      <details>
+        <summary className="text-lg font-semibold text-gray-800">
           {t("inventory.group", {
             groupId: group.groupId,
           })}
-        </h2>
+        </summary>
         <p className="text-sm text-gray-600">
           {t("inventory.groupDates", {
             start: new Date(group.start).toLocaleString(),
@@ -36,21 +36,21 @@ export function GroupInfo({
             coefficient: group.coefficient.toFixed(2),
           })}
         </p>
-        <hr className="my-2 border-gray-200" />
-        <p className="text-sm text-gray-500">
-          <Trans
-            t={t}
-            i18nKey="inventory.cart.stockLimit.maxCards"
-            values={{
-              classic: maxClassicCards,
-              holo: maxHoloCards,
-            }}
-            components={{
-              b: <span className="font-bold" />,
-            }}
-          />
-        </p>
-      </div>
+      </details>
+      <hr className="my-2 border-gray-200" />
+      <p className="text-sm text-gray-500">
+        <Trans
+          t={t}
+          i18nKey="inventory.cart.stockLimit.maxCards"
+          values={{
+            classic: maxClassicCards,
+            holo: maxHoloCards,
+          }}
+          components={{
+            b: <span className="font-bold" />,
+          }}
+        />
+      </p>
     </div>
   );
 }
